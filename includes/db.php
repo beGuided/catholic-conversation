b4705517e6a437945102219f9499d9dff1244456
beGuided <adejoh382@gmail.com>
2021-03-11 05:57:11 +0100

    connection to clearemysqlDB

--- includes/db.php
+++ includes/db.php
@@ -14,23 +14,23 @@
 // }
 
 
+
+
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("mysql://ba25a7fbb128b6:70da6f73@us-cdbr-east-03.cleardb.com/heroku_363dd4c0f1a785a?reconnect=true
-"));
-$cleardb_server = $cleardb_url["us-cdbr-east-03.cleardb.com"];
-$cleardb_username = $cleardb_url["ba25a7fbb128b6"];
-$cleardb_password = $cleardb_url["70da6f73"];
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-if(!$conn){
-    die('failed query'. mysqli_error($conn));
+if(!$connection){
+    die('failed query'. mysqli_error($connection));
 }else{
-  //  echo '<h1> connection success</h1>';
+  // echo '<h1> connection success</h1>';
 }
-
 
 ?>
